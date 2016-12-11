9ec83a32f98605389665edd556ff49df4aaf4b8c
Dmitry Mylnev <Dmitry.Mylnev@Markets.com>
2016-12-11 19:26:14 +0300

    finished task 1

--- plot1.R
+++ plot1.R
@@ -1,3 +1,21 @@
+require(dplyr)
+
 if(!exists("get_cdata", mode = "function")) source("get_data.R")
 
 D <- get_cdata()
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+
+trend <- D$NEI %>%
+    group_by(year) %>%
+    summarise(mean = mean(Emissions, na.rm = TRUE))
+
+
+png("plot1.png", width = 480, height = 480)
+
+plot(trend$year, trend$mean, main = "Total emissions from PM2.5 decreased in the United States", 
+     ylab = "PM2.5 emissions in the US, tons", xlab = "Year", type = "l")
+
+dev.off()
